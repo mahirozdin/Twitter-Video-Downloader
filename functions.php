6c5309cb810910e799e9ff529b49712267d62aec
Mahir Taha Özdin <ozdinmt@gmail.com>
2018-09-30 16:15:43 +0300

    Catching Diffrent Type of Videos
    $typeOfContent detects the type of content in the tweet_id and BIG IF this is a video/mp4 formatted data then returns video urls with their bitrates.

--- functions.php
+++ functions.php
@@ -17,7 +17,40 @@ function getTweetText($tweet){
 	return $tweet->full_text;
 }
 function getTweetVideo($tweet){
-	return $tweet->extended_entities->media[0]->video_info->variants[0]->url;
+	$sizeOfArray = count($tweet->extended_entities->media[0]->video_info->variants);
+	
+	$videoUrls = Array();
+	$videoUrlsIndexPoint = 0;
+	for($i = 0; $i< $sizeOfArray; $i++){
+		// can be video/mp4 , application/x-mpegURL
+		$typeOfContent = $tweet->extended_entities->media[0]->video_info->variants[$i]->content_type;
+		
+		if($typeOfContent == "video/mp4"){
+			$videoUrls[$videoUrlsIndexPoint]["bitrate"] = $tweet->extended_entities->media[0]->video_info->variants[$i]->bitrate;
+			$videoUrls[$videoUrlsIndexPoint]["url"] = cleanParametersFromUrl($tweet->extended_entities->media[0]->video_info->variants[$i]->url);
+		//Have to make index point+1 on each object added.
+		$videoUrlsIndexPoint++;
+		}
+	}
+	
+	return $videoUrls;
+}
+function cleanParametersFromUrl($url){
+	
+	// If URL has ? paramters. 
+	if(strpos($url, '?') !== false){
+		
+		//Catch and select first part.
+		$urlarray = explode("?",$url);
+		$newurl = $urlarray[0];
+		
+	}else{
+		
+		$newurl = $url;
+	
+	}
+	
+	return $newurl;
 }
 
 ?>
--- index.php
+++ index.php
@@ -5,21 +5,23 @@ require "functions.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 // API Connection Parameters.
-$consumer_key = "YOUR_CONSUMER_KEY";
-$consumer_secret_key = "YOUR_CONSUMER_SECRET";
+$consumer_key = "PKH803R9mSKnESzIHDYoslV4r";
+$consumer_secret_key = "sRrObGM0DA0IKjXl3JTmf7g7VzBCW3w8ajCPKVWrnW2hKCFzzK";
 
 // My tokens created by using Twitter Applications page create my tokens.
-$access_token = "YOUR_TOKEN";
-$access_token_secret = "YOUR_TOKEN_SECRET";
+$access_token = "891375250780377088-pa3CCCpxL1COetaXXZpqP3mPltWkyII";
+$access_token_secret = "gQzlTzFUX0K5GUZTzaOxjuUo2L72NWFLOhGy71lfOezNC";
 
 // Connection and getting permisions from API Connection parameters.
 $connection = new TwitterOAuth($consumer_key, $consumer_secret_key, $access_token, $access_token_secret);
 $content = $connection->get("account/verify_credentials");
 
-
+// https://twitter.com/CANBERK_dr/status/1046362761477926913
 // Getting Tweet Informations.
-$tweet_id = "1045676724770484224"; // Example_Tweet_id  get from :  https://twitter.com/TwitterMusic/status/1045676724770484224
+$tweet_id = "1046362761477926913"; // Example_Tweet_id  get from :  https://twitter.com/TwitterMusic/status/1045676724770484224
 $tweet = getTweetInfo($connection,$tweet_id);
+
+//print_r($tweet);
 
 //Get Tweet Text
 echo 'Tweet Text:'.getTweetText($tweet).'<br>';
@@ -28,7 +30,7 @@ echo 'Tweet Text:'.getTweetText($tweet).'<br>';
 echo 'Tweet Thumb:'.getTweetImage($tweet).'<br>';
 
 //Get Tweet Video URL
-echo 'Tweet Video URL:'.getTweetVideo($tweet);
+print_r(getTweetVideo($tweet));
 
 	
 	//	print_r ($tweet); // see all json formatted datas you can reach.
